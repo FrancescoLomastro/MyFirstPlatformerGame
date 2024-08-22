dbf01ad8b70014d2c2d8c833d95af939c5963d6f
Francesco Gregorio Lo Mastro <francescogregorio.lomastro@mail.polimi.it>
2024-08-22 11:14:33 +0200

    JavaDoc package Utility

--- PlatformerGame/src/main/java/org/example/Utility/HelpMethods.java
+++ PlatformerGame/src/main/java/org/example/Utility/HelpMethods.java
@@ -7,39 +7,62 @@ import java.awt.geom.Rectangle2D;
 
 import static org.example.Constants.Window.TILES_SIZE;
 
+/**
+ * This is a support class that contains some useful static methods that are used in the game
+ */
 public class HelpMethods {
 
-    public static float YPositionUnderRoofOrAboveFloor(Rectangle2D.Float hitbox, float movementSpeed) {
-        //hitbox.y è sempre una posizione accessibile, ma se vi aggiungessimo movementSpeed andremo
-        //a collidere col soffitto oppure col terreno
+    /**
+     * This method is used when the position of the hit box altered by verticalSpeed is not traversable.
+     * This can be caused because it is colliding with the ceiling or the floor.
+     * The method returns the new Y position of the hit box that, depending on the sign of verticalSpeed (Jump, Fall), puts
+     * the entity on the ground or below the ceiling
+     * @param hitbox The hit box of the entity
+     * @param verticalSpeed The vertical speed of the entity
+     * @return
+     */
+    public static float YPositionUnderRoofOrAboveFloor(Rectangle2D.Float hitbox, float verticalSpeed) {
         int currentTileY = (int) (hitbox.y / TILES_SIZE);
-        if (movementSpeed > 0) {
-            // Caduta sul pavimento
-            // La nuova posizione sarà la coordinata Y della casella corrente ma sfasata di un offset che ci assicura che
-            // i piedi del personaggio tocchino terra
+
+        if (verticalSpeed > 0) {
+            // Falling to the floor
+            // The new position will be a Y coordinate of the current tile but shifted by an offset that ensures that the
+            // character's feet touch the ground
             int yPosition = currentTileY * TILES_SIZE;
             int yOffset = (int) (TILES_SIZE - hitbox.height);
             return yPosition + yOffset - 1;
         } else {
-            // Salto e urto contro il soffitto
-            // La nuova posizione sarà la coordinata Y della casella corrente, il  che assicura che la testa del
-            // personaggio raggiunga il soffitto
+            // Jumping and collision with ceiling
+            // The new position will be a Y coordinate of the current tile, which ensures that the character's head touches the ceiling
             return currentTileY * TILES_SIZE;
         }
     }
 
-    public static float XPositionNextToWall(Rectangle2D.Float hitbox, float xSpeed) {
+    /**
+     * This method is used when the position of the hit box altered by horizontalSpeed is not traversable.
+     * This can be caused because it is colliding with a wall.
+     * @param hitbox The hit box of the entity
+     * @param horizontalSpeed The horizontal speed of the entity
+     * @return The new X position of the hit box that, depending on the sign of horizontalSpeed (Right, Left), puts the entity
+     */
+    public static float XPositionNextToWall(Rectangle2D.Float hitbox, float horizontalSpeed) {
         int currentTile = (int) (hitbox.x / TILES_SIZE);
-        if (xSpeed > 0) {
-            // Movimento a destra
+        if (horizontalSpeed > 0) {
+            // Right movement
             int tileXPos = currentTile * TILES_SIZE;
             int xOffset = (int) (TILES_SIZE - hitbox.width);
             return tileXPos + xOffset - 1;
         } else
-            // Movimento a sinistra
+            // Left movement
             return currentTile * TILES_SIZE;
     }
 
+    /**
+     * This method checks if the mouse is inside a button
+     * @param e The mouse event
+     * @param mb The button
+     * @return True if the mouse is inside the button, false otherwise
+     */
     public static boolean IsMouseIn(MouseEvent e, Button mb){
         return mb.getBounds().contains(e.getX(), e.getY());
     }
